491805d2cdda0f3847bb622f4f8447f02f4e6b4d
Felipe Agudelo Stouvenel <wfelipeagudelo@gmail.com>
2024-04-17 16:09:33 +0200

    WIP: transfer Lookup api calls

--- src/components/transferLookup/TransferLookup.tsx
+++ src/components/transferLookup/TransferLookup.tsx
@@ -13,7 +13,88 @@ import {
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 
+import useNetworkSelection from "../../../hooks/useNetworkSelection";
+import { useState } from "react";
+import { FieldValues, useForm } from "react-hook-form"
+
+// Interface for a single transfer inside a transaction
+interface Transfer {
+    account: string;
+    amount: number;
+    is_approval: boolean;
+}
+
+// Interface for a single Transaction
+interface Transaction {
+    transaction_id: string;
+    name: string;
+    transfers: Transfer[];
+    consensus_timestamp: string;
+    node: string;
+    result: string;
+    charged_tx_fee: number;
+    transaction_hash: string;
+}
+
 const TransferLookUp = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [transactions, setTransactions] = useState([]);
+    const [links, setLinks] = useState([]);
+    const { selectedNetwork } = useNetworkSelection();
+    // TODO add a hook to save the accountId same as network selection
+    const {
+        register,
+        handleSubmit,
+        formState: {errors},
+        setValue,
+    } = useForm<FieldValues>({
+        defaultValues: {
+            accountId: "",
+        }
+    });
+
+    // Checks updates the network value for the selected network
+    const getApiUrlForNetwork = (network: string) => {
+        switch(network){
+            case "mainnet":
+                return process.env.NEXT_PUBLIC_MAINNET;
+            case "testnet":
+                return process.env.NEXT_PUBLIC_TESTNET;
+            case "previewnet":
+                return process.env.NEXT_PUBLIC_PREVIEWNET;
+            default:
+                // TODO add error logging
+                throw new Error(`Select a Network: ${network}`);
+        }
+    };
+
+    const networkUrl = getApiUrlForNetwork(selectedNetwork);
+    /* 
+        Fetches the transactions based on an URL which is passed
+        handles errors if bad request and if unexpected error
+        returns data which has the Transaction interface model
+    */
+    async function fetchTransactions(url:string) {
+        try {
+            const response = await fetch(url);
+
+            if(!response.ok){
+                // TODO improve error logging
+                throw new Error(
+                    `Failed to fetch transactions. Status: ${response.status}`
+                );
+            }
+
+            const data = await response.json();
+            console.log(data);
+            return data || [];
+        } catch (error) {
+            // TODO improve error logging
+            console.error("Error fetching transactions: ", error);
+            throw error;
+        }
+    }
+
   return (
     <>
       <Card
