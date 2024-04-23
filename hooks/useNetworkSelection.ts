58639b1beb7b14232959054b57146ccdc479b742
Felipe Agudelo Stouvenel <wfelipeagudelo@gmail.com>
2024-04-23 13:23:08 +0200

    fix: formatting use network selection

--- hooks/useNetworkSelection.ts
+++ hooks/useNetworkSelection.ts
@@ -1,28 +1,28 @@
 import { useEffect, useState } from "react";
 
 const useNetworkSelection = () => {
-    /* 
+  /* 
         Checks if there is already an item in sessionStorage
         with the name selectedNetwork
     */
-    const [selectedNetwork, setSelectedNetwork] = useState<string>('mainnet');
+  const [selectedNetwork, setSelectedNetwork] = useState<string>("mainnet");
 
-    useEffect(() => {
-        const storedNetwork = sessionStorage.getItem('selectedNetwork');
-        if(storedNetwork){
-            setSelectedNetwork(storedNetwork)
-        }
-    }, []);
-
-    // Sets the stored network to the new network value
-    const updateNetworkSelection = (network: string) => {
-        setSelectedNetwork(network)
-        if (typeof window !== 'undefined'){
-            sessionStorage.setItem('selectedNetwork', network);
-        }
+  useEffect(() => {
+    const storedNetwork = sessionStorage.getItem("selectedNetwork");
+    if (storedNetwork) {
+      setSelectedNetwork(storedNetwork);
     }
+  }, []);
 
-    return { selectedNetwork, updateNetworkSelection }
-}
+  // Sets the stored network to the new network value
+  const updateNetworkSelection = (network: string) => {
+    setSelectedNetwork(network);
+    if (typeof window !== "undefined") {
+      sessionStorage.setItem("selectedNetwork", network);
+    }
+  };
 
-export default useNetworkSelection
+  return { selectedNetwork, updateNetworkSelection };
+};
+
+export default useNetworkSelection;
