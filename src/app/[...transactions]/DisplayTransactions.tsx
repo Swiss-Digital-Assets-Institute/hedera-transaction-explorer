723360559dd82c40698e7bcf2d997ced77ae509c
Felipe Agudelo Stouvenel <wfelipeagudelo@gmail.com>
2024-04-24 12:54:08 +0200

    fix: delted extra comment

--- src/app/[...transactions]/DisplayTransactions.tsx
+++ src/app/[...transactions]/DisplayTransactions.tsx
@@ -57,7 +57,6 @@ const DisplayTransaction = () => {
       case "previewnet":
         return process.env.NEXT_PUBLIC_PREVIEWNET;
       default:
-        // TODO add error logging
         throw new Error(`Select a Network: ${network}`);
     }
   };
